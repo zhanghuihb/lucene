07ffff2fc5f94cae84200684cafeea8d3a9dcff0
zhanghuihb <zhanghuihb@126.com>
2020-03-17 16:46:10 +0800

    create repository

---
+++ src/main/java/com/thdays/lucene/LuceneApplication.java
@@ -0,0 +1,13 @@
+package com.thdays.lucene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuceneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuceneApplication.class, args);
+    }
+
+}
